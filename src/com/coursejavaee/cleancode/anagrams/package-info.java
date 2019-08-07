95d1ff38b75905ad054811b7929cfc54d00836b1
Georgich88 <georgeisaev@gmail.com>
2019-08-07 21:44:16 +0700

    Complete task#1

---
+++ src/com/coursejavaee/cleancode/anagrams/package-info.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2019 Georgich88
+ * This program is made available under the terms of the MIT License.
+ */
+
+/**
+ * Anagrams solver (task 1)
+ * Foxminded Java EE course.
+ */
+
+/**
+ * @author Georgy Isaev
+ * @version 1.02 2019-08-07
+ */
+package com.coursejavaee.cleancode.anagrams;
