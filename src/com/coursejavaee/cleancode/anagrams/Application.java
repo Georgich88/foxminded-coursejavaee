4a2eb74161ffe2e6c146af89df50df092920c5a0
Georgich88 <georgeisaev@gmail.com>
2019-08-10 19:50:54 +0700

    Code review #2.  - DELIMITER;  - New class to manage AnagramMaker.

---
+++ src/com/coursejavaee/cleancode/anagrams/Application.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+package com.coursejavaee.cleancode.anagrams;
+
+import java.util.Scanner;
+
+/**
+ * The class that tests anagram creation
+ * 
+ * @version 1.02 2019-08-07
+ * @author Georgy Isaev
+ */
+public class Application {
+
+    /**
+     * Tests anagram creation via standard input
+     * 
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter sentence:");
+        String text = scanner.nextLine();
+        scanner.close();
+        System.out.println(AnagramMaker.makeAnagram(text));
+
+    }
+
+}
