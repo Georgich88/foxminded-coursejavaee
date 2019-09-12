e150f79052850e82cd4576547e06672e993f9abd
Georgich88 <georgeisaev@gmail.com>
2019-09-12 08:33:30 +0700

    Code review #2. - metod names, refactoring.

--- src/main/java/com/foxminded/rodin/anagram/AnagramMakerTest.java
+++ src/main/java/com/foxminded/rodin/anagram/AnagramMakerTest.java
@@ -1,83 +1,83 @@
 package com.foxminded.rodin.anagram;
 
-import static org.junit.Assert.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
 public class AnagramMakerTest {
 
     @Test
-    public void shouldReturnAnagramIfGetCorrectSentences() throws Exception {
+    public void shouldRevertSentence() {
 
         String sentence = "abcd efgh a1bcd efg!h";
         String expectedResult = "dcba hgfe d1cba hgf!e";
 
         String result = AnagramMaker.makeAnagram(sentence);
+
         assertEquals(expectedResult, result);
     }
 
     @Test
-    public void shouldReturnNotAnagramOnlyNumbersIfGetOnlyNumbers() throws Exception {
+    public void shouldNotRevertNumbers() {
 
         String sentence = "3.14 23,2 2,7182818284590452353602874713527 12.3456";
         String expectedResult = "3.14 23,2 2,7182818284590452353602874713527 12.3456";
 
         String result = AnagramMaker.makeAnagram(sentence);
+
         assertEquals(expectedResult, result);
     }
 
     @Test
-    public void shouldReturnSentenceWithoutWordsIfGetSentenceWithoutWords() throws Exception {
+    public void shouldNotRevertSpecialCharacters() {
 
         String sentence = "!@#$%^ @#_+~~~~~ 7$$<>{}";
         String expectedResult = "!@#$%^ @#_+~~~~~ 7$$<>{}";
 
         String result = AnagramMaker.makeAnagram(sentence);
+
         assertEquals(expectedResult, result);
 
     }
 
     @Test
-    public void shouldReturnNotEnglishAnagramIfGetNotEnglishSentence() throws Exception {
+    public void shouldNotRevertNotLatinSymbols() {
 
-        String sentence = "목에 하얀 수건을 둘러놓고 얼굴을 씻겨주던";
-        String expectedResult = "에목 얀하 을건수 고놓러둘 을굴얼 던주겨씻";
-
-        String result = AnagramMaker.makeAnagram(sentence);
-        assertEquals(expectedResult, result);
-    }
-
-    @Test
-    public void shouldReturnNullPointerExceptionIfGetNullArgument() throws NullPointerException {
-        try {
-            String result = AnagramMaker.makeAnagram(null);
-            fail("Check null exeption");
-        } catch (NullPointerException e) {
-
-        }
-
-    }
-
-    @Test
-    public void shouldReturnNotAnagramIfGetWordsDividedByTab() throws Exception {
-
-        String tabDelimiter = "\t";
-
-        String sentence = "This" + tabDelimiter + "text" + tabDelimiter + "is" + tabDelimiter + "divided" + tabDelimiter
-                + "by" + tabDelimiter + "tabs";
-        String expectedResult = "sihT" + tabDelimiter + "txet" + tabDelimiter + "si" + tabDelimiter + "dedivid"
-                + tabDelimiter + "yb" + tabDelimiter + "sbat";
+        String sentence = "维基百科，自由";
+        String expectedResult = "科百基维，由自";
 
         String result = AnagramMaker.makeAnagram(sentence);
+
         assertNotEquals(expectedResult, result);
     }
 
     @Test
-    public void shouldReturnEmptyStringIfGetEmptyString() throws Exception {
+    public void shouldReturnNullPointerExceptionIfGetNullArgument() {
+
+        assertThrows(NullPointerException.class, () -> {
+            String result = AnagramMaker.makeAnagram(null);
+        });
+
+    }
+
+    @Test
+    public void shouldMakeWrongAnagramFromSenteceDividedByTab() {
+
+        String sentence = "This\ttext\ttabDelimiter\tdivided\tby\ttabs";
+        String expectedResult = "sihT\ttxet\tsi\tdedivid\tyb\tsbat";
+
+        String result = AnagramMaker.makeAnagram(sentence);
+
+        assertNotEquals(expectedResult, result);
+    }
+
+    @Test
+    public void shouldReturnEmptyStringIfGetEmptyString() {
 
         String resultAnagram = AnagramMaker.makeAnagram("");
+
         assertEquals(resultAnagram, "");
 
     }
