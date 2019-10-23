974abebf35354fe653b332b5f1fbf70176c8907e
Georgich88 <georgeisaev@gmail.com>
2019-10-23 20:44:22 +0700

    Code review #1.  - Add new class to handle file readings;  - Add new constants for default log file names;  - Delete unnessesary fields start and end for Lap class;  - Refactoring.

--- src/main/java/com/foxminded/rodin/formulaone/Application.java
+++ src/main/java/com/foxminded/rodin/formulaone/Application.java
@@ -4,13 +4,19 @@ import java.io.IOException;
 
 public class Application {
 
+    private static final String LOG_START_DEFAULT_NAME = "start.log";
+    private static final String LOG_END_DEFAULT_NAME = "end.log";
+    private static final String LOG_ABBREVIATION_DEFAULT_NAME = "abbreviations.txt";
+    private static final int DEFAULT_TOP_LAPS_NUMBER = 15;
+
     public static void main(String[] args) {
 
         try {
-            String report = FastestRacersReport.getFastestRacersReport("start.log", "end.log", "abbreviations.txt", 15);
+            String report = FastestRacersReport.getFastestRacersReport(LOG_START_DEFAULT_NAME, LOG_END_DEFAULT_NAME,
+                    LOG_ABBREVIATION_DEFAULT_NAME, DEFAULT_TOP_LAPS_NUMBER);
             System.out.println(report);
         } catch (IOException error) {
-            error.printStackTrace();
+            System.out.println(error.getMessage());
         }
 
     }
--- src/main/java/com/foxminded/rodin/formulaone/Lap.java
+++ src/main/java/com/foxminded/rodin/formulaone/Lap.java
@@ -1,19 +1,13 @@
 package com.foxminded.rodin.formulaone;
 
 import java.time.Duration;
-import java.time.LocalDateTime;
 
 public class Lap implements Comparable<Lap> {
 
-    private LocalDateTime start;
-    private LocalDateTime stop;
     private Duration duration;
     private Racer racer;
 
-    public Lap(LocalDateTime start, LocalDateTime stop, Duration duration, Racer racer) {
-        super();
-        this.start = start;
-        this.stop = stop;
+    public Lap(Duration duration, Racer racer) {
         this.duration = duration;
         this.racer = racer;
     }
@@ -23,22 +17,6 @@ public class Lap implements Comparable<Lap> {
         if (o == null)
             return -1;
         return duration.compareTo(o.getDuration());
-    }
-
-    public LocalDateTime getStart() {
-        return start;
-    }
-
-    public void setStart(LocalDateTime start) {
-        this.start = start;
-    }
-
-    public LocalDateTime getStop() {
-        return stop;
-    }
-
-    public void setStop(LocalDateTime stop) {
-        this.stop = stop;
     }
 
     public Duration getDuration() {
